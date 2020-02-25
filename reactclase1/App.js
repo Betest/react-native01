b406d455b232a45da7553c7b538b76ce5cbd02ad
505 <505@academicos.cesde.edu.co>
2020-02-24 21:32:15 -0500

    second commit

--- reactclase1/App.js
+++ reactclase1/App.js
@@ -4,15 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+
       <View style={styles.box1}>
-        <Text>cesde!</Text>
+        <View style={styles.box3}>
+          <Text>Ejemplo 1</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text>Ejemplo 2</Text>
+        </View>
       </View>
       <View style={styles.box2}>
-        <Text>Caja2!</Text>
+        <View style={styles.box4}>
+          <Text>CESDE</Text>
+        </View>
+        <View style={styles.box5}>
+          <Text>Ejemplo 4</Text>
+        </View>
       </View>
-      <View style={styles.box3}>
-        <Text>Caja 3!</Text>
-      </View>
+
     </View>
 
   );
@@ -21,19 +30,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e91e63',
-    flexDirection : 'column'
+    backgroundColor: '#fff',
+    flexDirection: 'column'
   },
+
   box1: {
     flex: 1,
-    backgroundColor: '#64dd17',
+    backgroundColor: '#1de9b6',
+    flexDirection: 'row'
   },
   box2: {
-    flex: 2,
-    backgroundColor: '#8e9',
+    flex: 1,
+    backgroundColor: '#ffea00',
+    flexDirection: 'row'
+
   },
   box3: {
     flex: 1,
-    backgroundColor: '#8e9',
+    backgroundColor: '#2979ff',
+  },
+  box4: {
+    flex: 1,
+    backgroundColor: '#e91e63'
+  },
+  box5: {
+    flex: 1,
+    backgroundColor: '#e65100'
+  },
+  box6: {
+    flex: 1,
+    backgroundColor: '#8d6e63'
   },
 });
